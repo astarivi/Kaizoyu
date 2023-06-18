105e8a58224613fe604eaf3e9a900c2852dd5b58
astarivi <astarivi@aol.com>
2023-06-18 13:29:32 -0500

    Hide System UI correctly, when user is watching media

--- app/src/main/java/com/astarivi/kaizoyu/video/VideoPlayerActivity.java
+++ app/src/main/java/com/astarivi/kaizoyu/video/VideoPlayerActivity.java
@@ -252,13 +252,11 @@ public class VideoPlayerActivity extends AppCompatActivityTheme {
     @Override
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-        hideSystemUI();
     }
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
-        if (hasFocus) hideSystemUI();
     }
 
     @Override
@@ -268,22 +266,6 @@ public class VideoPlayerActivity extends AppCompatActivityTheme {
         }
 
         return super.onTouchEvent(event);
-    }
-
-    private void hideSystemUI(){
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-//            WindowInsetsControllerCompat windowInsetsController = WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView());
-//            windowInsetsController.hide(WindowInsetsCompat.Type.systemBars());
-//        } else {
-//            getWindow().getDecorView().setSystemUiVisibility(
-//                    View.SYSTEM_UI_FLAG_FULLSCREEN
-//                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-//                            | View.SYSTEM_UI_FLAG_IMMERSIVE
-//                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-//                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-//            );
-//        }
     }
 
     private void delayedExit(){
