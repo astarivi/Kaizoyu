5801f5835ea8192778dc9a0f28aa80b7db240d60
astarivi <astarivi@aol.com>
2023-06-25 18:40:43 -0500

    Prepare for update

--- app/src/main/java/com/astarivi/kaizoyu/core/updater/UpdateManager.java
+++ app/src/main/java/com/astarivi/kaizoyu/core/updater/UpdateManager.java
@@ -30,8 +30,8 @@ import okhttp3.HttpUrl;
 
 public class UpdateManager {
     // TODO Change this for every release, doh.
-    public static String VERSION = "0.3";
-    public static String VERSION_NAME = "fool";
+    public static String VERSION = "0.4";
+    public static String VERSION_NAME = "magician";
 
     @ThreadedOnly
     public @Nullable LatestUpdate getLatestUpdate() throws ParseException {
