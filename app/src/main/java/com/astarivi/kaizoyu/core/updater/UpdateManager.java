11316090ce61e64cf13ea75f95d3addca5ef21c0
astarivi <astarivi@aol.com>
2023-07-24 07:53:31 -0500

    Release 0.702-BETA

--- app/src/main/java/com/astarivi/kaizoyu/core/updater/UpdateManager.java
+++ app/src/main/java/com/astarivi/kaizoyu/core/updater/UpdateManager.java
@@ -31,7 +31,7 @@ import okhttp3.HttpUrl;
 
 public class UpdateManager {
     public static String VERSION = BuildConfig.VERSION_NAME;
-    public static String VERSION_NAME = "urania";
+    public static String VERSION_NAME = "cubic";
 
     public static boolean isBeta() {
         return VERSION.contains("-BETA");
